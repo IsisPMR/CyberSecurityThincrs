d0ef7c6dbecfef1828999b57228e5cca7e26dc00
IsisPMR <59493619+IsisPMR@users.noreply.github.com>
2023-05-13 13:27:38 -0600

    Log in creates a session.
    The code creates a session when the user log in, working on destroy this session after some time without activity from the user on the web, but not yet there.

--- admin.php
+++ admin.php
@@ -7,5 +7,18 @@
 </head>
 <body>
     <h1> hola admin</h1>
+    <form action ="process.php" method="post" >
+	<input type="submit" value="Salir"/>
+    </form>
 </body>
 </html>
+
+<?php
+//Validacion por tiempo
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 10)) {
+    // last request was more than 30 minutes ago
+    session_unset();     // unset $_SESSION variable for the run-time 
+    session_destroy();   // destroy session data in storage
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+ ?>
