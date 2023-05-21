f318aa38ec102fcb03fd63ef2c8bc342b7518f8e
IsisPMR <59493619+IsisPMR@users.noreply.github.com>
2023-05-21 00:39:45 -0600

    Log in session can be closed by two ways.
    This code allows the user not only to close the session with the "Log out" button as the previous version, but now the session will be close automatically after 10 seconds of inactivity, if the user refresh the page the user will be redirected to the Log in page to sign in again with the credentials.

--- admin.php
+++ admin.php
@@ -14,11 +14,14 @@
 </html>
 
 <?php
+session_start();
 //Validacion por tiempo
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 10)) {
     // last request was more than 30 minutes ago
     session_unset();     // unset $_SESSION variable for the run-time 
     session_destroy();   // destroy session data in storage
+    header("Location: index.html");
 }
 $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
  ?>
+ 
